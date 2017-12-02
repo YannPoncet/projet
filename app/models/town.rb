3d675d082d3259239c7e122754e8473b1021879b
yann.poncetpro <yann.poncetpro@gmail.com>
2017-12-02 06:19:07 -0500

    Mise a jour des tests et ajout de VCR

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,11 +1,14 @@
 class Town < ActiveRecord::Base
   
   before_validation :load_position
+  validates :lat, :lon, presence: true
 
   private
   def load_position
-    places = Nominatim.search(name).limit(1)
-    self.lat = places.first.lat
-    self.lon = places.first.lon
+    places = Nominatim.search(self.name).limit(1).first
+    if places
+      self.lat = places.lat
+      self.lon = places.lon
+    end
   end
 end
